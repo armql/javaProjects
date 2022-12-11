c7e133c28f6621233b69cf08dc182f8da3deb1d3
flyingsaucr <am57839@ubt-uni.net>
2022-12-11 12:04:03 +0100

    *

---
+++ interface-exceptions/Angazhim.java
@@ -0,0 +1,12 @@
+
+package interface-exceptions;
+
+public interface Angazhim {
+    public String getAngazhimi() {
+        return angazhimi;
+    }
+
+    public boolean mentoron() {
+        return true;
+    }
+}
